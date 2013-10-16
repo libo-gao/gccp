a95f9e024c5704e1c9b47c955b0b1726ac045d44
Xiao Jia <stfairy@gmail.com>
2013-10-16 10:41:05 +0800

    add testcases

--- src/gc/test/TestMain.java
+++ src/gc/test/TestMain.java
@@ -1,29 +1,62 @@
 package gc.test;
 
+import java.lang.reflect.Constructor;
+
 import gc.CopyCollectHeap;
 import gc.Heap;
+import gc.InsufficientMemory;
 import gc.MarkSweepHeap;
 import gc.RefCountHeap;
 
 public class TestMain {
-	private void run(String caseName, Heap heap, TestCase test) {
+	private void run(TestCase test, Class<? extends Heap> heapClass, int size) {
+		String testName = test.getClass().getSimpleName();
+		String heapName = heapClass.getSimpleName();
+		String caseName = testName + "@" + heapName;
+
 		try {
-			test.run(heap);
-			System.out.println("✔ " + caseName);
+			Constructor<? extends Heap> C = heapClass.getConstructor(int.class);
+			Heap heapOfSize = C.newInstance(size);
+
+			long startTime = System.currentTimeMillis();
+			test.run(heapOfSize);
+			long elapsed = System.currentTimeMillis() - startTime;
+
+			try {
+				Heap heapOfSizeMinusOne = C.newInstance(size - 1);
+				test.run(heapOfSizeMinusOne);
+				System.out.println("? " + caseName);
+			} catch (InsufficientMemory e) {
+				System.out.println("✔ " + caseName + " (" + elapsed + "ms)");
+			}
 		} catch (Exception e) {
 			System.out.println("✘ " + caseName);
 		}
 	}
 
 	public void run() {
-		run("node normal", new Heap(300), new TestNode(100));
-		run("node refcount", new RefCountHeap(204), new TestNode(100));
-		run("node marksweep", new MarkSweepHeap(204), new TestNode(100));
-		run("node copycollect", new MarkSweepHeap(204), new TestNode(100));
+		run(new TestNode(100), Heap.class, 300);
+		run(new TestNode(100), RefCountHeap.class, 204);
+		run(new TestNode(100), MarkSweepHeap.class, 204);
+		run(new TestNode(100), CopyCollectHeap.class, 408);
 
-		run("cycle normal", new Heap(100000), new TestCycle(50));
-		run("cycle marksweep", new MarkSweepHeap(104), new TestCycle(50));
-		run("cycle copycollect", new CopyCollectHeap(208), new TestCycle(50));
+		run(new TestChain(10, 1000), Heap.class, 10000000);
+		run(new TestChain(10, 1000), RefCountHeap.class, 12000);
+		run(new TestChain(10, 1000), MarkSweepHeap.class, 12000);
+		run(new TestChain(10, 1000), CopyCollectHeap.class, 24000);
+
+		run(new TestCycle(50), Heap.class, 100000);
+		run(new TestCycle(50), MarkSweepHeap.class, 104);
+		run(new TestCycle(50), CopyCollectHeap.class, 208);
+
+		run(new TestLoop(10, 1000), Heap.class, 10000000);
+		run(new TestLoop(10, 1000), MarkSweepHeap.class, 12000);
+		run(new TestLoop(10, 1000), CopyCollectHeap.class, 24000);
+
+		run(new TestFragmentation(), Heap.class, 5993);
+		run(new TestFragmentation(), RefCountHeap.class, 4009);
+		run(new TestFragmentation(), MarkSweepHeap.class, 6009);
+		run(new TestFragmentation(), CopyCollectHeap.class, 6018);
 	}
 
 	public static void main(String[] args) {
